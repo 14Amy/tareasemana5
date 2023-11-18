9cb7d5f1d43764f5a6aabdf27bf02802ba0717e2
Amy <acarrera1@utpl.edu.ec>
2023-11-18 01:12:57 -0500

    actualizacion de archivos

--- agregar_registros.py
+++ agregar_registros.py
@@ -1,7 +1,8 @@
 from sqlalchemy.orm import sessionmaker
 # se importa la clase(s) del
 # archivo crear_entidades
-from crear_entidades import Autor
+from crear_entidades import Ciudades
+from crear_entidades import Estadio
 # se importa el engine
 from base_datos import engine
 
@@ -15,20 +16,26 @@ Session = sessionmaker(bind=engine) # Se usa el engine
 session = Session()
 
 # se crea un objetos de tipo Autor
-autor1 = Autor(nombre="José", apellido="Armijos", nacionalidad="ecuatoriana",
-        edad=40)
-autor2 = Autor(nombre="Sara", apellido="Benitez", nacionalidad="colombiana",
-        edad=20)
-autor3 = Autor(nombre="Pedro", apellido="Díaz", nacionalidad="peruana",
-        edad=35)
-autor4 = Autor(nombre="Mónica", apellido="Carrión", nacionalidad="ecuatoriana",
-        edad=25)
+ciudad1 = Ciudades(nombreciudad="quito", pais="ecuador", continente="america")
+ciudad2 = Ciudades(nombreciudad="riobamba", pais="ecuador", continente="america")
+ciudad3 = Ciudades(nombreciudad="pillaro", pais="ecuador", continente="america")
+ciudad4 = Ciudades(nombreciudad="bolivar", pais="ecuador", continente="america")
+
+estadio1 = Estadio(nombreestadio="liga", ciudad="quito", direccion="america")
+estadio2 = Estadio(nombreestadio="barcelona", ciudad="guayaquil", direccion="america")
+estadio3 = Estadio(nombreestadio="olimpico", ciudad="quito", direccion="america")
+estadio4 = Estadio(nombreestadio="aucas", ciudad="quito", direccion="america")
 # se agrega los objetos de tipo Autor a la sesión
 # a la espera de un commit
-session.add(autor1)
-session.add(autor2)
-session.add(autor3)
-session.add(autor4)
+session.add(ciudad1)
+session.add(ciudad2)
+session.add(ciudad3)
+session.add(ciudad4)
+
+session.add(estadio1)
+session.add(estadio2)
+session.add(estadio3)
+session.add(estadio4)
 
 # se necesita confirmar los cambios que existan en la sessió
 # se usar commit
